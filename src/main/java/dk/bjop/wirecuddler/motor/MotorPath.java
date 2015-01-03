91a77dfc61e2d318f84132c53f2bf0663d2ee51d
Bjorn Petersen <bjop@bjop.dk>
2015-01-03 10:57:08 +0100

    Rename MotorPath

--- src/main/java/dk/bjop/wirecuddler/motor/MotorPath.java
+++
@@ -1,9 +0,0 @@
-package dk.bjop.wirecuddler.motor;
-
-/**
- * Created by bpeterse on 10-09-2014.
- */
-public interface MotorPath {
-    public int getExpectedTachoPosAtTimeT(long elapsedTimeMillis, int controllerID);
-    public boolean isMovementFinished(long elapsedTimeMillis);
-}
---
+++ src/main/java/dk/bjop/wirecuddler/motor/MotorPathMove.java
@@ -0,0 +1,13 @@
+package dk.bjop.wirecuddler.motor;
+
+import dk.bjop.wirecuddler.math.XYZCoord;
+
+/**
+ * Created by bpeterse on 10-09-2014.
+ */
+public interface MotorPathMove {
+    public int[] getExpectedTachoPosAtTimeT(long elapsedTimeMillis);
+    public boolean isMovementFinished(long elapsedTimeMillis);
+    public void setStartPos(XYZCoord startPos);
+    public XYZCoord getTargetPos();
+}
